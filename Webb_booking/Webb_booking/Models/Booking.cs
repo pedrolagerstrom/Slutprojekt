304d0a70ef730dd00f1d8ac9c1bbd5fdee7b622c
Pedro Lagerström <pedro.lagerstrom@student.kyh.se>
2020-11-16 14:27:47 +0100

    Fixat create.html och delete.html i booking och index.html i home samt lite css fix

--- Webb_booking/Webb_booking/Models/Booking.cs
+++ Webb_booking/Webb_booking/Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
@@ -10,10 +11,19 @@ namespace Webb_booking.Models
     public class Booking
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Booker { get; set; }
         public Guid BirdId { get; set; }
+
+        [Required]
         public string BirdName { get; set; }
+
+        [Required]
         public DateTime From { get; set; }
+
+        [Required]
         public DateTime To { get; set; }
     }
 }
--- Webb_booking/Webb_booking/Models/ViewModels/CreateBookingViewModel.cs
+++ Webb_booking/Webb_booking/Models/ViewModels/CreateBookingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +9,10 @@ namespace Webb_booking.Models.ViewModels
 {
     public class CreateBookingViewModel
     {
+        [Required]
         public Booking Booking { get; set; }
+
+        [Required]
         public List<Bird> Birds { get; set; }
     }
 }
